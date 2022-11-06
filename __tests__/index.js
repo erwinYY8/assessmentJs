316154e76664ec826a8fadc6e77b8ccb17b89624
erwinYY8 <yueyong2021@163.com>
2022-11-07 01:22:15 +0800

    add: finish

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,34 +1,161 @@
-import * as utils from '../index'
+
+const utils = require('../index')
 
 describe("problem 1 (stripPrivateProperties)", () => {
-  test("should strip private properties", () => {
+  test("strip private properties", () => {
     expect(
       utils.stripPrivateProperties(
-        ["password", "token"],
+        ["numberId", "age"],
         [
           {
-            name: "stan",
-            email: "stan@smith.test",
-            password: "secret",
-            token: "123",
+            userName: "jokey",
+            email: "jokey@mail.com",
+            numberId: "123456",
+            age: 29
           },
           {
-            name: "fran",
-            email: "sran@smith.test",
-            password: "secret",
-            token: "123",
+            userName: "kuky",
+            email: "kuky@mail.com",
+            numberId: "12345678",
+            age: 35
           },
         ]
       )
     ).toEqual([
       {
-        name: "stan",
-        email: "stan@smith.test",
+        userName: "jokey",
+        email: "jokey@mail.com"
       },
       {
-        name: "fran",
-        email: "sran@smith.test",
+        userName: "kuky",
+        email: "kuky@mail.com"
       },
     ]);
+  });
+});
+
+
+describe("problem 2 (excludeByProperty)", () => {
+  test("exclude by the specified property", () => {
+    expect(
+      utils.excludeByProperty("exclude", [
+        { color: "red" },
+        { color: "green" },
+        { color: "blue", exclude: true },
+        { color: "pink" }
+      ])
+    ).toEqual([
+      { color: "red" },
+      { color: "green" },
+      { color: "pink" }
+    ]);
+  });
+});
+
+
+describe("problem 3 (sumDeep)", () => {
+  test("should calculate the total of `num` from `groups`", () => {
+    expect(
+      utils.sumDeep([
+        {
+          groups: [{ num: 1 }, { num: 4 }, { num: 6 }],
+        },
+        {
+          groups: [{ num: 0 }, { num: 1 }, { num: 3 }],
+        },
+      ])
+    ).toEqual([{ groups: 11 }, { groups: 4 }]);
+  });
+});
+
+
+describe("problem 4 (applyStatusColor)", () => {
+  test("a collection of apply status color", () => {
+    expect(
+      utils.applyStatusColor(
+        {
+          blank: [100, 200],
+          blue: [300, 301],
+        },
+        [
+          {
+            status: 100,
+          },
+          {
+            status: 200,
+          },
+          {
+            status: 300,
+          },
+          {
+            status: 301,
+          },
+          {
+            status: 300,
+          },
+          {
+            status: 302,
+          },
+        ]
+      )
+    ).toEqual([
+      {
+        status: 100,
+        color: "blank",
+      },
+      {
+        status: 200,
+        color: "blank",
+      },
+      {
+        status: 300,
+        color: "blue",
+      },
+      {
+        status: 301,
+        color: "blue",
+      },
+      {
+        status: 300,
+        color: "blue",
+      },
+    ]);
+  });
+});
+
+
+describe('problem 5 (createGreeting)', () => {
+  test('It is more convenient to greet by means of create a function', () => {
+    const greet = (greeting, name) => `${greeting} ${name}`;
+    const sayThanks = utils.createGreeting(greet, 'thanks');
+    const sayPolite = utils.createGreeting(greet, 'polite');
+
+    expect(sayThanks('jokey')).toEqual('thanks jokey');
+    expect(sayPolite('kuky')).toEqual('polite kuky');
+  });
+});
+
+describe("problem 6 (setDefaults)", () => {
+  test("ensure has that default props", () => {
+    const applyDefaults = utils.setDefaults({ promotion: true });
+
+    expect(
+      applyDefaults({
+        name: "jokey",
+        promotion: false,
+      })
+    ).toEqual({
+      name: "jokey",
+      promotion: false,
+    });
+
+    expect(
+      applyDefaults({
+        name: "kuky",
+      })
+    ).toEqual({
+      name: "kuky",
+      promotion: true,
+    });
   });
 });
--- index.js
+++ index.js
@@ -1,101 +1,24 @@
-const stripPrivateProperties = (props, data) => {
+exports.stripPrivateProperties = (props, data) => {
   data.forEach(item => props.forEach(p => delete item[p]))
   return data
 }
 
-// const res = stripPrivateProperties(['a', 'b'], [
-//   {
-//     a: '1',
-//     b: 2,
-//     c: 3,
-//     d: 4
-//   },
-//   {
-//     a: '1',
-//     b: 2,
-//     e: 3,
-//     f: 4
-//   }
-// ])
-// console.log(res, 'res');
 
-
-const excludeByProperty = (prop, data) => {
+exports.excludeByProperty = (prop, data) => {
   return data.filter(item => !item[prop])
 }
 
 
-const data = [
-  {
-    items: [{ val: 1 }, { val: 2 }, { val: 3 }]
-  },
-  {
-    items: [{ val: 0 }, { val: 1 }]
-  }
-]
-
-// [{ items: 6 }, { items: 1 }]
-const sumDeep = (data) => {
+exports.sumDeep = (data) => {
   data.forEach(item => {
-    const sum = item['items'].reduce((pre, cur) => pre + cur.val, 0)
-    item['items'] = sum
+    const sum = item['groups'].reduce((pre, cur) => pre + cur.num, 0)
+    item['groups'] = sum
   })
   return data
 }
 
-const sum = sumDeep(data)
-console.log(sum, 'sum');
 
-
-const a = {
-  red: [404, 400],
-  green: [200, 201],
-}
-const b = [
-  {
-    status: 404,
-  },
-  {
-    status: 200,
-  },
-  {
-    status: 404,
-  },
-  {
-    status: 201,
-  },
-  {
-    status: 400,
-  },
-  {
-    status: 408,
-  },
-]
-
-const r = [
-  {
-    status: 404,
-    color: "red",
-  },
-  {
-    status: 200,
-    color: "green",
-  },
-  {
-    status: 404,
-    color: "red",
-  },
-  {
-    status: 201,
-    color: "green",
-  },
-  {
-    status: 400,
-    color: "red",
-  },
-]
-
-const applyStatusColor = (a, b) => {
+exports.applyStatusColor = (a, b) => {
   const statusMap = new Map
   Object.keys(a).forEach(color => {
     a[color].forEach(status => {
@@ -103,14 +26,15 @@ const applyStatusColor = (a, b) => {
     })
   })
 
-  const defaultColor = 'default'
+  const def = 'def'
   return b.map(m => {
-    m.color = statusMap.get(m.status) || defaultColor
+    m.color = statusMap.get(m.status) || def
     return m
   })
-  .filter(f => {
-    return f.color !== defaultColor
-  })
+  .filter(f => f.color !== def)
 }
 
-console.log(applyStatusColor(a, b), 'applyStatusColor');
+exports.createGreeting = (greetFunc, greeting) => name => greetFunc(greeting, name);
+
+
+exports.setDefaults = () => obj => Object.assign({ promotion: true }, obj);
